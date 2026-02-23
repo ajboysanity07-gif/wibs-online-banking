efab3c3e38b8b040004d88048611ecd21323c5b3
ajboysanity07-gif <ajboysanity07@gmail.com>
2026-02-23 16:33:00 +0800

    Align auth password brokers and admin provider

--- config/auth.php
+++ config/auth.php
@@ -15,7 +15,7 @@ return [
 
     'defaults' => [
         'guard' => env('AUTH_GUARD', 'web'),
-        'passwords' => env('AUTH_PASSWORD_BROKER', 'users'),
+        'passwords' => env('AUTH_PASSWORD_BROKER', 'appusers'),
     ],
 
     /*
@@ -101,6 +101,12 @@ return [
     'passwords' => [
         'appusers' => [
             'provider' => 'appusers',
+            'table' => env('AUTH_PASSWORD_RESET_TOKEN_TABLE', 'password_reset_tokens'),
+            'expire' => 60,
+            'throttle' => 60,
+        ],
+        'adminusers' => [
+            'provider' => 'adminusers',
             'table' => env('AUTH_PASSWORD_RESET_TOKEN_TABLE', 'password_reset_tokens'),
             'expire' => 60,
             'throttle' => 60,
